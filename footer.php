5455c0486b69f291e0d10ec5be8ad90474877987
Chinh202 <badboy9b@gmail.com>
2016-10-06 01:52:01 +0700

    Revert ""
    This reverts commit 0d93384e66ab2c8c0c0847cc3ce7a1014949ab14.

--- footer.php
+++ footer.php
@@ -1,11 +1,11 @@
 <?php ?>
 
 </div>
-<div class="container"> 
-    <div class="col-lg-12" style="text-align: center;background-color: #d71921">        
-            <p style=" margin:10px 0;padding: 0;color: #fff">Copyright &COPY; 2016 MinhChausc.com</p>            
+<footer class="footer" style="text-align: center;background-color: #d71921"> 
+    <div class="container">
+        <p style=" margin:10px 0;color: #fff">Copyright &COPY; 2016 MinhChausc.com</p>
     </div>
-</div>                   
+</footer>                   
 <!--</div>-->
 </div>
 </body>
